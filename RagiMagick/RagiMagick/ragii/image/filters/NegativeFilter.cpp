431adef83469cb9dacdcc7221861721f45097a55
ragingo <save.the.world.crush@gmail.com>
2019-03-13 18:33:13 +0900

    余計な配列を使わないよう修正

--- RagiMagick/RagiMagick/ragii/image/filters/NegativeFilter.cpp
+++ RagiMagick/RagiMagick/ragii/image/filters/NegativeFilter.cpp
@@ -18,15 +18,14 @@ void NegativeFilter::apply()
 		return;
 	}
 
-	uint8_t a[16] = { 0xff };
-
 	__m128i src_a, src_b, dst;
-	src_b = _mm_load_si128(reinterpret_cast<__m128i*>(a));
+	src_b = _mm_set1_epi8(0xff);
 
+	// 一度に 4px (BGRA 32bit * 4 = 128 bit) 処理する
 	for (int i = 0; i < w * h; i += 4)
 	{
 		src_a = _mm_load_si128(reinterpret_cast<__m128i*>(img));
-		dst = _mm_add_epi16(src_b, -src_a);
+		dst = _mm_sub_epi16(src_b, src_a);
 		_mm_store_si128(reinterpret_cast<__m128i*>(img), dst);
 		img += 16;
 	}
