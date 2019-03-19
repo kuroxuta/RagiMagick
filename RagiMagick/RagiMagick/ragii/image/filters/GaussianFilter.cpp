406b74bc81754bc16dd0234a64dc8ecf191771b3
ragingo <save.the.world.crush@gmail.com>
2019-03-19 16:42:56 +0900

    コード整理

--- RagiMagick/RagiMagick/ragii/image/filters/GaussianFilter.cpp
+++ RagiMagick/RagiMagick/ragii/image/filters/GaussianFilter.cpp
@@ -8,13 +8,16 @@ using namespace ragii::image;
 
 namespace {
 
-    constexpr std::array<float, 9> SmallKernel = {
+    const int SmallKernelSize = 9;
+    const int LargeKernelSize = 25;
+
+    constexpr array<float, SmallKernelSize> SmallKernel = {
         1.0f/16.0f, 2.0f/16.0f, 1.0f/16.0f,
         2.0f/16.0f, 4.0f/16.0f, 2.0f/16.0f,
         1.0f/16.0f, 2.0f/16.0f, 1.0f/16.0f
     };
 
-    constexpr std::array<float, 25> LargeKernel = {
+    constexpr array<float, LargeKernelSize> LargeKernel = {
         1.0f/256.0f,  4.0f/256.0f,  6.0f/256.0f,  4.0f/256.0f, 1.0f/256.0f,
         4.0f/256.0f, 16.0f/256.0f, 24.0f/256.0f, 16.0f/256.0f, 4.0f/256.0f,
         6.0f/256.0f, 24.0f/256.0f, 36.0f/256.0f, 24.0f/256.0f, 6.0f/256.0f,
@@ -22,78 +25,129 @@ namespace {
         1.0f/256.0f,  4.0f/256.0f,  6.0f/256.0f,  4.0f/256.0f, 1.0f/256.0f,
     };
 
-	struct Color
-	{
-		int b;
-		int g;
-		int r;
-	};
+    // 基準ピクセルからの行オフセット(3x3)
+    constexpr array<int, SmallKernelSize> SmallKernelRowOffsets = {
+        -1, -1, -1,
+         0,  0,  0,
+         1,  1,  1
+    };
 
-	Color getColor(uint8_t* img, int width, int depth, int row, int col)
-	{
-		return
-			Color {
-				*(img + (row * width * depth + col + 0)),
-				*(img + (row * width * depth + col + 1)),
-				*(img + (row * width * depth + col + 2))
-			};
-	}
+    // 基準ピクセルからの行オフセット(5x5)
+    constexpr array<int, LargeKernelSize> LargeKernelRowOffsets = {
+        -2, -2, -2, -2, -2,
+        -1, -1, -1, -1, -1,
+         0,  0,  0,  0,  0,
+         1,  1,  1,  1,  1,
+         2,  2,  2,  2,  2
+    };
 
-	void setColor(uint8_t* img, int width, int depth, int row, int col, const Color& color)
-	{
-		*(img + (row * width * depth + col + 0)) = color.b;
-		*(img + (row * width * depth + col + 1)) = color.g;
-		*(img + (row * width * depth + col + 2)) = color.r;
-	}
+    // 基準ピクセルからの列オフセット(3x3)
+    constexpr array<int, SmallKernelSize> SmallKernelColOffsetsBGR = {
+        -3, 0, 3,
+        -3, 0, 3,
+        -3, 0, 3
+    };
+
+    // 基準ピクセルからの列オフセット(3x3)
+    constexpr array<int, SmallKernelSize> SmallKernelColOffsetsBGRA = {
+        -4, 0, 4,
+        -4, 0, 4,
+        -4, 0, 4
+    };
+
+    // 基準ピクセルからの列オフセット(5x5)
+    constexpr array<int, LargeKernelSize> LargeKernelColOffsetsBGR = {
+        -6, -3, 0, 3, 6,
+        -6, -3, 0, 3, 6,
+        -6, -3, 0, 3, 6,
+        -6, -3, 0, 3, 6,
+        -6, -3, 0, 3, 6
+    };
+
+    // 基準ピクセルからの列オフセット(5x5)
+    constexpr array<int, LargeKernelSize> LargeKernelColOffsetsBGRA = {
+        -8, -4, 0, 4, 8,
+        -8, -4, 0, 4, 8,
+        -8, -4, 0, 4, 8,
+        -8, -4, 0, 4, 8,
+        -8, -4, 0, 4, 8
+    };
+
+    struct Color
+    {
+        int b;
+        int g;
+        int r;
+    };
+
+    Color getColor(uint8_t* img, int width, int depth, int row, int col)
+    {
+        return
+            Color {
+                *(img + (row * width * depth + col + 0)),
+                *(img + (row * width * depth + col + 1)),
+                *(img + (row * width * depth + col + 2))
+            };
+    }
+
+    void setColor(uint8_t* img, int width, int depth, int row, int col, const Color& color)
+    {
+        *(img + (row * width * depth + col + 0)) = color.b;
+        *(img + (row * width * depth + col + 1)) = color.g;
+        *(img + (row * width * depth + col + 2)) = color.r;
+    }
 
 }
 
 void GaussianFilter::apply()
 {
-	int w = m_Params.width;
-	int h = m_Params.height;
-	int d = m_Params.bitCount / 8;
-	uint8_t* img = m_Params.image;
+    int w = m_Params.width;
+    int h = m_Params.height;
+    int d = m_Params.bitCount / 8;
+    uint8_t* img = m_Params.image;
 
-	if (/*d != 3 && */d != 4)
-	{
-		cout << "depth " << d << " not supported." << endl;
-		return;
-	}
+    if (/*d != 3 && */d != 4)
+    {
+        cout << "depth " << d << " not supported." << endl;
+        return;
+    }
 
-	// 基準ピクセルからの行オフセット
-	int rowOffsets[] = { -1, -1, -1, 0, 0, 0, 1,  1,  1 };
-	// 基準ピクセルからの列オフセット
-	int bgrColOffsets[] = { -3, 0, 3, -3, 0, 3, -3, 0, 3 };
-	// 基準ピクセルからの列オフセット
-	int bgraColOffsets[] = { -4, 0, 4, -4, 0, 4, -4, 0, 4 };
+    int row, col = 0;
+    int i = 0;
+    Color tempColor = {};
+    Color resultColor = {};
 
-	const int* colOffsets = d == 3 ? bgrColOffsets : bgraColOffsets;
+// TODO: 何かで判断して実行時に切り替えたい
+#define USE_SMALL_KERNEL 0
+#if USE_SMALL_KERNEL
+    const auto& kernel = SmallKernel;
+    const auto& rowOffsets = SmallKernelRowOffsets;
+    const auto& colOffsets = d == 3 ? SmallKernelColOffsetsBGR : SmallKernelColOffsetsBGRA;
+#else
+    const auto& kernel = LargeKernel;
+    const auto& rowOffsets = LargeKernelRowOffsets;
+    const auto& colOffsets = d == 3 ? LargeKernelColOffsetsBGR : LargeKernelColOffsetsBGRA;
+#endif
 
-	int row, col = 0;
-	int i = 0;
-	Color tempColor = {};
-	Color resultColor = {};
+    for (row = 1; row < h - 1; row++)
+    {
+        for (col = d; col < w * d - d; col += d)
+        {
+            resultColor = {};
 
-	for (row = 1; row < h - 1; row++)
-	{
-		for (col = d; col < w * d - d; col += d)
-		{
-			resultColor = {};
+            for (i = 0; i < kernel.size(); i++)
+            {
+                tempColor = std::move(getColor(img, w, d, row + rowOffsets[i], col + colOffsets[i]));
+                resultColor.b += (tempColor.b * kernel[i]);
+                resultColor.g += (tempColor.g * kernel[i]);
+                resultColor.r += (tempColor.r * kernel[i]);
+            }
 
-			for (i = 0; i < SmallKernel.size(); i++)
-			{
-				tempColor = std::move(getColor(img, w, d, row + rowOffsets[i], col + colOffsets[i]));
-				resultColor.b += (tempColor.b * SmallKernel[i]);
-				resultColor.g += (tempColor.g * SmallKernel[i]);
-				resultColor.r += (tempColor.r * SmallKernel[i]);
-			}
+            resultColor.b = std::clamp(resultColor.b, 0x00, 0xff);
+            resultColor.g = std::clamp(resultColor.g, 0x00, 0xff);
+            resultColor.r = std::clamp(resultColor.r, 0x00, 0xff);
 
-			resultColor.b = std::clamp(resultColor.b, 0x00, 0xff);
-			resultColor.g = std::clamp(resultColor.g, 0x00, 0xff);
-			resultColor.r = std::clamp(resultColor.r, 0x00, 0xff);
-
-			setColor(img, w, d, row, col, resultColor);
-		}
-	}
+            setColor(img, w, d, row, col, resultColor);
+        }
+    }
 }
