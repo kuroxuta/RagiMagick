e6ddc7662fded085c74361e9e72b84359dae6624
ragingo <save.the.world.crush@gmail.com>
2019-03-21 21:30:31 +0900

    ビルド時に真っ先にコードフォーマッタが実行されるようにした

---
+++ RagiMagick/CodeFormat.cmake
@@ -0,0 +1,15 @@
+
+function(format_code target_name)
+
+    find_program(CLANG_FORMAT_EXE clang-format)
+    if(NOT CLANG_FORMAT_EXE)
+        return()
+    endif()
+
+    file(GLOB_RECURSE MY_ALL_SOURCES *.h *.c *.cpp)
+    add_custom_target(
+        ${target_name}
+        COMMAND "${CLANG_FORMAT_EXE}" -style=file -i ${MY_ALL_SOURCES}
+    )
+
+endfunction()
