d2d772588ef5645c89a2e9626ed0d811a4bfc530
ragingo <save.the.world.crush@gmail.com>
2019-03-23 02:53:23 +0900

    libjpeg-turbo を submodule として取り込み＆ 自分のコードだけ clang-format の対象になるよう修正＆libjpeg-turbo の不必要なプロジェクトがIDEに表示されないようにした

--- RagiMagick/CodeFormat.cmake
+++ RagiMagick/CodeFormat.cmake
@@ -6,7 +6,11 @@ function(format_code target_name)
         return()
     endif()
 
-    file(GLOB_RECURSE MY_ALL_SOURCES *.h *.c *.cpp)
+    file(GLOB_RECURSE MY_ALL_SOURCES
+        include/*.h src/*.c src/*.cpp
+        lib/ragii/*.h lib/ragii/*.c lib/ragii/*.cpp
+        lib/ragii-image/*.h lib/ragii-image/*.c lib/ragii-image/*.cpp
+    )
     add_custom_target(
         ${target_name}
         COMMAND "${CLANG_FORMAT_EXE}" -style=file -i ${MY_ALL_SOURCES}
